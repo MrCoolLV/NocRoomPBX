b4b4e431f0c10f3305889fd73281cdb0f628981e
MrCoolLV <ivanjr@nocroom.com>
2018-12-04 17:52:29 -0800

    Update database.php

--- resources/classes/database.php
+++ resources/classes/database.php
@@ -1,29 +1,5 @@
 <?php
-/*
-	FusionPBX
-	Version: MPL 1.1
 
-	The contents of this file are subject to the Mozilla Public License Version
-	1.1 (the "License"); you may not use this file except in compliance with
-	the License. You may obtain a copy of the License at
-	http://www.mozilla.org/MPL/
-
-	Software distributed under the License is distributed on an "AS IS" basis,
-	WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-	for the specific language governing rights and limitations under the
-	License.
-
-	The Original Code is FusionPBX
-
-	The Initial Developer of the Original Code is
-	Mark J Crane <markjcrane@fusionpbx.com>
-	Copyright (C) 2010 - 2017
-	All Rights Reserved.
-
-	Contributor(s):
-	Mark J Crane <markjcrane@fusionpbx.com>
-	Luis Daniel Lucio Quiroz <dlucio@okay.com.mx>
-*/
 include "root.php";
 
 //define the database class
@@ -83,12 +59,12 @@ include "root.php";
 							include $_SERVER["PROJECT_ROOT"]."/resources/config.php";
 						} elseif (file_exists($_SERVER["PROJECT_ROOT"]."/resources/config.php")) {
 							include $_SERVER["PROJECT_ROOT"]."/resources/config.php";
-						} elseif (file_exists("/etc/fusionpbx/config.php")){
+						} elseif (file_exists("/etc/nocroompbx/config.php")){
 							//linux
-							include "/etc/fusionpbx/config.php";
-						} elseif (file_exists("/usr/local/etc/fusionpbx/config.php")) {
+							include "/etc/nocroompbx/config.php";
+						} elseif (file_exists("/usr/local/etc/nocroompbx/config.php")) {
 							//bsd
-							include "/usr/local/etc/fusionpbx/config.php";
+							include "/usr/local/etc/nocroompbx/config.php";
 						}
 
 					//backwards compatibility
